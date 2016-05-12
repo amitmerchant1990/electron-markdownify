30923d84e1f9ce7028d66e33106031a3c157b9b4
Amit Merchant <bullredeyes@gmail.com>
2016-05-12 17:31:28 +0530

    Fix indentation

--- main.js
+++ main.js
@@ -33,32 +33,32 @@ function createWindow () {
   //Set native menubar
   var template = [
 		{
-			label: "File",
-			submenu: [
-				{label: "Quit", accelerator: "Command+Q", click: app.quit}
-			]
-		},
-		{
-			label: "Edit",
-			submenu: [
-				{label: "Undo", accelerator: "CmdOrCtrl+Z", role: "undo"},
-				{label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", role: "redo"},
-				{type: "separator"},
-				{label: "Cut", accelerator: "CmdOrCtrl+X", role: "cut"},
-				{label: "Copy", accelerator: "CmdOrCtrl+C", role: "copy"},
-				{label: "Paste", accelerator: "CmdOrCtrl+V", role: "paste"},
-				{label: "Select All", accelerator: "CmdOrCtrl+A", role: 'selectall'}
-			]
-		},
-		{
-			label: "Help",
-			submenu: [
-				{label: "About", accelerator: "CmdOrCtrl+P", click: function () {
+      label: "File",
+      submenu: [
+        {label: "Quit", accelerator: "Command+Q", click: app.quit}
+      ]
+    },
+    {
+      label: "Edit",
+      submenu: [
+        {label: "Undo", accelerator: "CmdOrCtrl+Z", role: "undo"},
+        {label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", role: "redo"},
+        {type: "separator"},
+        {label: "Cut", accelerator: "CmdOrCtrl+X", role: "cut"},
+        {label: "Copy", accelerator: "CmdOrCtrl+C", role: "copy"},
+        {label: "Paste", accelerator: "CmdOrCtrl+V", role: "paste"},
+        {label: "Select All", accelerator: "CmdOrCtrl+A", role: 'selectall'}
+      ]
+    },
+    {
+      label: "Help",
+      submenu: [
+        {label: "About", accelerator: "CmdOrCtrl+P", click: function () {
           dialog.showMessageBox({title: "About Markdownify", type:"info", message: "A minimal Markdown Editor desktop app. \nMIT Copyright (c) 2016 Amit Merchant <bullredeyes@gmail.com>", buttons: ["Close"] });
-				}}
-			]
-		}
-	];
+        }}
+      ]
+    }
+  ];
 
 	Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 }
