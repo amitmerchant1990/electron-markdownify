0b9c1d3a018bac69e3ce23462f16300de3c39402
Amit Merchant <bullredeyes@gmail.com>
2016-05-12 14:10:13 +0530

    Add support to preview raw HTML

--- js/app.js
+++ js/app.js
@@ -3,15 +3,35 @@
 * Copyright (c) 2016 Amit Merchant <bullredeyes@gmail.com>
  */
 
+var showdown  = require('showdown');
+
 window.onload = function() {
   var plainText = document.getElementById('plainText');
   var markdownArea = document.getElementById('markdown');
 
-  var convertTextAreaToMarkdown = function() {
+  var convertTextAreaToMarkdownAndHTML = function() {
+    //Md -> Preview
     var markdownText = plainText.value;
     html = marked(markdownText);
     markdownArea.innerHTML = html;
+
+    //Md -> HTML
+    converter = new showdown.Converter();
+    html      = converter.makeHtml(markdownText);
+    document.getElementById("htmlPreview").value = html;
   }
 
-  plainText.addEventListener('input', convertTextAreaToMarkdown);
+  plainText.addEventListener('input', convertTextAreaToMarkdownAndHTML);
+}
+
+var currentValue = 0;
+function clkPref(opt) {
+    currentValue = opt.value;
+    if ( currentValue=='preview' ) {
+      document.getElementById("htmlPreview").style.display = "none";
+      document.getElementById("markdown").style.display = "block";
+    } else if ( currentValue=='html' ) {
+      document.getElementById("markdown").style.display = "none";
+      document.getElementById("htmlPreview").style.display = "block";
+    }
 }
