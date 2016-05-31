701eb7293ad7ab02300b658b4819d04fbae30ff9
Amit Merchant <bullredeyes@gmail.com>
2016-05-31 16:16:54 +0530

    Commented out oprnDevTools()

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ function createWindow () {
   mainWindow.loadURL(mainPage);
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  //mainWindow.webContents.openDevTools();
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function() {
