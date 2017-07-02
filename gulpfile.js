d0a29fe7574e5392f63c23d8c1b832f6f9e534e5
Amit Merchant <bullredeyes@gmail.com>
2017-07-03 00:18:23 +0530

    Updated task build.

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ const packager = require('electron-packager');
 
 const config = JSON.parse(fs.readFileSync('package.json'));
 const appVersion = config.version;
-const electronVersion = config.devDependencies['electron-prebuilt'].match(/[\d.]+/)[0];
+const electronVersion = config.devDependencies['electron'].match(/[\d.]+/)[0];
 const options = {
 	asar: true,
 	dir: '.',
@@ -24,22 +24,30 @@ gulp.task('build:osx', (done) => {
 	options.platform = 'darwin';
 	options['app-bundle-id'] = 'com.amitmerchant.markdownify';
 	options['helper-bundle-id'] = 'com.amitmerchant.markdownify.helper';
-	
+
 	packager(options, (err, paths) => {
 		if (err) {
 			console.error(err);
 		}
-		
+
 		done();
 	});
 });
 
 gulp.task('build:linux', () => {
-	// @TODO 
+	// @TODO
 });
 
 gulp.task('build:windows', () => {
-	// @TODO 
+	options.arch = 'x64';
+	options.platform = 'win32';
+	packager(options, (err, paths) => {
+		if (err) {
+			console.error(err);
+		}
+
+		done();
+	});
 });
 
 gulp.task('build', ['build:osx', 'build:linux', 'build:windows']);
